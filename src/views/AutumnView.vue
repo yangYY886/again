ef5ac1f0fd4092b3e28ffc170871e14d52e7504d
yangYY886 <2842862459qq.com>
2024-05-20 10:32:49 +0800

    第一次提交

--- src/views/AutumnView.vue
+++ src/views/AutumnView.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
 </template>
 
 <script>
@@ -10,7 +7,7 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
+  name: 'AutumnView',
   components: {
     HelloWorld
   }
